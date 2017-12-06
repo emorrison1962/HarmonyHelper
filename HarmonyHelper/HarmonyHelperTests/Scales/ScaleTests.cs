964f52a9f68bc58eef42ed5baa11566a2a478eed
emorrison1962 <eric.morrison@gmail.com>
2017-12-06 00:25:47 -0600

    120617

---
+++ HarmonyHelper/HarmonyHelperTests/Scales/ScaleTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Eric.Morrison.Harmony.Tests
+{
+    [TestClass()]
+    public class ScaleTests
+    {
+        [TestMethod()]
+        public void ScaleTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void todoTest()
+        {
+
+            "TODO"
+#if false
+
+            Chord = Bb7;
+            foreach (var scale in scaleThesaurus)
+            {
+                if (scale.Contains(chord)) //implies implicit IEnumerable<NoteName> conversion operator!!!
+                {
+                    Debug.Write(scale.Name.ToString());
+                    Debug.Write(scale.ToString());
+                    Debug.Write(" contains: ");
+                    Debug.Write(chord.Name.ToString());
+                }
+            }
+
+#endif
+            Assert.Fail();
+        }
+
+
+    }//class
+}//ns
