caaedf57b3a7ee3e5d2ed954364db82f5a221182
DL\emorrison <eric.morrison@waspbarcode.com>
2022-08-18 14:35:26 -0500

    duration.

---
+++ HarmonyHelper/HarmonyHelper/Duration/Duration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eric.Morrison.Harmony.Duration
+{
+    public class Duration
+    {
+        public enum DurationEnum
+        {
+            Whole = Quarter * 4,
+            Half = Quarter * 2,
+            Quarter = 240,
+            Eighth = Quarter / 2,
+            Sixteenth = Eighth / 2,
+            SixtyFourth = Sixteenth / 2,
+
+            EighthTriplet = Quarter / 3,
+            SixteenthTriplet = Eighth / 3,
+
+            EightQuintuplet = Half / 5,
+            SixteenthQuintuplet = Quarter / 5,
+        }
+
+        public DurationEnum Value { get; set; }
+
+        public Duration(DurationEnum duration)
+        {
+            this.Value = duration;
+        }
+
+    }//class
+}//ns
