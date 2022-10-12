197529b424dcc9294190bf127ba4990eb8a17298
emorrison <emorrison@DL69Q0NN3>
2022-10-12 11:26:26 -0500

    MusicXmlParser

---
+++ HarmonyHelper/HarmonyHelper/Analysis/ReHarmonizer/MusicXmlParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Eric.Morrison.Harmony.Chords;
+
+namespace Eric.Morrison.Harmony
+{
+    public class MusicXmlParser
+    {
+        public MusicXmlParser()
+        {
+
+        }
+
+        MusicXmlParingResult Parse(string filename)
+        {
+            return null;
+        }
+
+    }//class
+    public class MusicXmlParingResult
+    { 
+        public List<Bar> Bars { get; set; }
+    }//class
+
+    public class Bar
+    { 
+        public List<ChordFormula> Chords { get; set; }
+        public List<NoteName> Notes { get; set; }
+    }
+}//ns
