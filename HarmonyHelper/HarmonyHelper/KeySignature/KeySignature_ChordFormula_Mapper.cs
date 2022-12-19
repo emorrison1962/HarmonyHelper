0a2dc90077ff442c76fd2f51d823378508f223d3
emorrison1962 <Eric.Morrison@gmail.com>
2022-12-19 00:07:45 -0600

    121922 02

--- HarmonyHelper/HarmonyHelper/KeySignature/KeySignature_ChordFormula_Mapper.cs
+++ HarmonyHelper/HarmonyHelper/KeySignature/KeySignature_ChordFormula_Mapper.cs
@@ -13,7 +13,7 @@ namespace Eric.Morrison.Harmony
     public class KeySignature2ChordFormulaMap
     {
         public Dictionary<KeySignature, List<ChordFormula>> 
-            KeyToFormulaMaps { get; set; } 
+            KeySignatureToChordFormulaMaps { get; set; } 
                 = new Dictionary<KeySignature, List<ChordFormula>>();
 
         public KeySignature2ChordFormulaMap()
@@ -24,14 +24,14 @@ namespace Eric.Morrison.Harmony
                 {
                     if (IsDiatonicEnum.Yes == key.IsDiatonic(formula.NoteNames))
                     {
-                        if (this.KeyToFormulaMaps.TryGetValue(key, out var formulas))
+                        if (this.KeySignatureToChordFormulaMaps.TryGetValue(key, out var formulas))
                         {
                             formulas.Add(formula);
                         }
                         else
                         {
-                            this.KeyToFormulaMaps[key] = new List<ChordFormula>();
-                            this.KeyToFormulaMaps[key].Add(formula);
+                            this.KeySignatureToChordFormulaMaps[key] = new List<ChordFormula>();
+                            this.KeySignatureToChordFormulaMaps[key].Add(formula);
                         }
                     }
                 }
@@ -52,7 +52,7 @@ namespace Eric.Morrison.Harmony
 
         public List<ChordFormula> GetChordFormulas(KeySignature key)
         {
-            var result = this.KeyToFormulaMaps[key]
+            var result = this.KeySignatureToChordFormulaMaps[key]
                 .OrderBy(x => x.Root)
                 .ThenBy(x => x.NoteNames.Count)
                 .ToList();
@@ -64,7 +64,7 @@ namespace Eric.Morrison.Harmony
             var set = new HashSet<ChordFormula>();
             foreach (var key in keys)
             {
-                this.KeyToFormulaMaps[key]
+                this.KeySignatureToChordFormulaMaps[key]
                     .ForEach(x => set.Add(x));
             }
             
@@ -77,7 +77,7 @@ namespace Eric.Morrison.Harmony
     public class ChordFormula2KeySignatureMap
     {
         public Dictionary<ChordFormula, List<KeySignature>>
-            FormulaToKeyMaps { get; set; }
+            ChordFormulaToKeySignatureMaps { get; set; }
                 = new Dictionary<ChordFormula, List<KeySignature>>();
 
 
@@ -89,14 +89,14 @@ namespace Eric.Morrison.Harmony
                 {
                     if (IsDiatonicEnum.Yes == key.IsDiatonic(formula.NoteNames))
                     {
-                        if (this.FormulaToKeyMaps.TryGetValue(formula, out var dict))
+                        if (this.ChordFormulaToKeySignatureMaps.TryGetValue(formula, out var dict))
                         {
                             dict.Add(key);
                         }
                         else
                         {
-                            this.FormulaToKeyMaps[formula] = new List<KeySignature>();
-                            this.FormulaToKeyMaps[formula].Add(key);
+                            this.ChordFormulaToKeySignatureMaps[formula] = new List<KeySignature>();
+                            this.ChordFormulaToKeySignatureMaps[formula].Add(key);
                         }
                     }
                 }
@@ -116,7 +116,7 @@ namespace Eric.Morrison.Harmony
 
         public List<KeySignature> GetKeys(TimedEvent<ChordFormula> chord)
         {
-            var result = this.FormulaToKeyMaps[chord.Event]
+            var result = this.ChordFormulaToKeySignatureMaps[chord.Event]
                 .OrderBy(x => x.NoteName)
                 .ToList();
             return result;
