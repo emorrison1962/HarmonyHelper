4a4e8500da7e34f92b50960f54596d34c7b8395a
emorrison <eric.morrison@waspbarcode.com>
2024-02-12 14:33:23 -0600

    021224

--- HarmonyHelper/HarmonyHelperTests/Composition/MelodyFactoryTests.cs
+++ HarmonyHelper/HarmonyHelperTests/Composition/MelodyFactoryTests.cs
@@ -22,24 +22,10 @@ namespace HarmonyHelper.Composition.Tests
         [TestMethod()]
         public void CreateTest()
         {
-
-            var MAX = MotifDirectionEnum.None 
-                | MotifDirectionEnum.Ascending 
-                | MotifDirectionEnum.Descending;
-
-            for (int i = (int)MotifDirectionEnum.Unknown; i < (int)MAX; i++) 
-            {
-                var e = (MotifDirectionEnum)i;
-                var msg =
-                $"{(e.HasFlag(MotifDirectionEnum.Descending) ? 1 : 0)}" +
-                $"{(e.HasFlag(MotifDirectionEnum.Ascending) ? 1 : 0)}" +
-                $"{(e.HasFlag(MotifDirectionEnum.None) ? 1 : 0)}";
-                Debug.WriteLine(msg);
-            }
-            new object();
-
             var seq = ChordSequenceTests.CreateChordSequence();
             MelodyFactory.Create(seq);
+
+            new object();
         }
     }
 }
